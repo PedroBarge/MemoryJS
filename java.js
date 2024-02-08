1db67f834fa115cc1284dc9455ba4f492f88d233
PedroBarge <pedro525tavares@gmail.com>
2024-02-08 08:45:42 +0000

    handler dont work

--- java.js
+++ java.js
@@ -13,6 +13,86 @@ let images = [
 ];
 images = images.concat(images);
 
+let cardsObjct = [
+  {
+    id: 1,
+    name: "BarMinder",
+    imageFront: "Images/BarMinder.png",
+    imageBack: "Images/cardBack.webp",
+  },
+  {
+    id: 2,
+    name: "BatMinder",
+    imageFront: "Images/BatMinder.png",
+    imageBack: "Images/cardBack.webp",
+  },
+  {
+    id: 3,
+    name: "CowMinder",
+    imageFront: "Images/CowMinder.png",
+    imageBack: "Images/cardBack.webp",
+  },
+  {
+    id: 4,
+    name: "FuzzyMinder",
+    imageFront: "Images/FuzzyMinder.png",
+    imageBack: "Images/cardBack.webp",
+  },
+  {
+    id: 5,
+    name: "AutumnMinder",
+    imageFront: "Images/AutumnMinder.png",
+    imageBack: "Images/cardBack.webp",
+  },
+  {
+    id: 6,
+    name: "Bigodes",
+    imageFront: "Images/Bigodes.png",
+    imageBack: "Images/cardBack.webp",
+  },
+  {
+    id: 7,
+    name: "MinderGamer",
+    imageFront: "Images/MinderGamer.png",
+    imageBack: "Images/cardBack.webp",
+  },
+  {
+    id: 8,
+    name: "FemaleMinder",
+    imageFront: "Images/FemaleMinder.png",
+    imageBack: "Images/cardBack.webp",
+  },
+  {
+    id: 9,
+    name: "GentlemanMinder",
+    imageFront: "Images/GentlemanMinder.png",
+    imageBack: "Images/cardBack.webp",
+  },
+  {
+    id: 10,
+    name: "MinderBiker",
+    imageFront: "Images/MinderBiker.png",
+    imageBack: "Images/cardBack.webp",
+  },
+];
+
+cardsObjct = cardsObjct.concat(cardsObjct);
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+cardsObjct = shuffle(cardsObjct);
+
 let gameBoard = ["", "", "", "", "", "", "", "", ""];
 
 function renderBoard() {
@@ -21,19 +101,38 @@ function renderBoard() {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     cell.dataset.index = i;
-    cell.textContent = gameBoard[i];
+
+    const img = document.createElement("img");
+    img.src = cardsObjct[i].imageBack;
+
+    cell.appendChild(img);
+
     cell.addEventListener("click", handleCellClick);
+
     board.appendChild(cell);
   }
 }
 
 function handleCellClick(event) {
-  const index = event.target.dataset.index;
+  let index = event.target.dataset.index;
+  console.log("entrou no handleCellClick");
+  let currentCard = cardsObjct[index];
 
   if (gameBoard[index] === "") {
-    gameBoard[index];
-    renderBoard();
+    let img = event.target.querySelector("img");
+    let imgSrcFileName = img.src.split("/").pop(); // Obtém apenas o nome do arquivo da imagem atual
+    let imageBackFileName = currentCard.imageBack.split("/").pop(); // Obtém apenas o nome do arquivo da imagem de costas
+
+    if (imgSrcFileName === imageBackFileName) { // Compara os nomes dos arquivos
+      img.src = currentCard.imageFront;
+      console.log("entrou on if");
+    } else {
+      img.src = currentCard.imageBack;
+      console.log("entrou no else");
+    }
   }
+  console.log("saiu do handleCellClick");
 }
+
 
 renderBoard();
